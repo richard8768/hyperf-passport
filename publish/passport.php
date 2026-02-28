3bf34297e8154097ebc81610fa915c1bd6dea487
richard8768 <jobsen123@qq.com>
2026-02-28 21:16:52 +0800

    Code modification

--- publish/passport.php
+++ publish/passport.php
@@ -9,13 +9,13 @@ declare(strict_types=1);
  * @license  https://github.com/richard8768/hyperf-passport/blob/master/LICENSE
  */
 return [
-    'session_user_login_uri' => env('PASSPORT_SESSION_USER_LOGIN_URL', '/user_login'),
+    'session_user_login_uri' => \Hyperf\Support\env('PASSPORT_SESSION_USER_LOGIN_URL', '/user_login'),
     'key_store_path' => 'storage',
     'client_uuids' => false,
-    'key' => env('PASSPORT_KEY', ''),
+    'key' => \Hyperf\Support\env('PASSPORT_KEY', ''),
     'token_days' => null,
     'refresh_token_days' => null,
     'person_token_days' => null,
-    'database_connection' => env('DB_CONNECTION', 'default'),
+    'database_connection' => \Hyperf\Support\env('DB_CONNECTION', 'default'),
     'is_revoke_user_others_token' => true, // when user login if revoke user's all token except current one
 ];
